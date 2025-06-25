f0efc31c6ceea06fdf016d44019792c761e0b940
Hiro <kawaikoan0206@gmail.com>
2025-06-25 12:19:12 +0900

    Fix29

--- models.py
+++ models.py
@@ -13,6 +13,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(256))
     is_admin = db.Column(db.Boolean, nullable=False, default=False)
+    show_in_ranking = db.Column(db.Boolean, nullable=False, default=True)
     is_confirmed = db.Column(db.Boolean, nullable=False, default=False)
     confirmed_on = db.Column(db.DateTime, nullable=True)    
     answers = db.relationship('UserAnswer', backref='user', lazy='dynamic')
